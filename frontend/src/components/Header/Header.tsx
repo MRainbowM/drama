148ebe2fa3820dee51126d985329af4b6d3401de
Мария Фалилеева <falileeva.ma@gmail.com>
2024-09-20 17:36:15 +0800

    header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -5,15 +5,26 @@ import Image from 'next/image'
 export default function Header() {
     return (
         <div className={styles.header}>
-            <Image src={`/static/images/nd.png`} alt='НД' width="100" height="100" />
+            <Image src={`/static/images/nd.png`} alt='НД' width="50" height="50" />
+            
+            <div className={styles.burger}>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+            </div>
 
-            <nav className={styles.menu}>
+
+            {/* <nav className={styles.menu}>
                 <ul>
-                    <li><a href="#">Главная</a></li>
+                    <li><a href="#">Афиша</a></li>
                     <li><a href="#">О нас</a></li>
+                    <li><a href="#">Коллектив</a></li>
+                    <li><a href="#">С.А.М.</a></li>
+                    <li><a href="#">Ресторан</a></li>
+                    <li><a href="#">Yoga</a></li>
                     <li><a href="#">Контакты</a></li>
                 </ul>
-            </nav>
+            </nav> */}
         </div>
     )
 }
