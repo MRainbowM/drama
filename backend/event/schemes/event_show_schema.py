5acc5210d89dc648c7655dd8906f0e2694b6f685
Мария Фалилеева <falileeva.ma@gmail.com>
2024-10-06 00:01:54 +0800

    fix

--- backend/event/schemes/event_show_schema.py
+++ backend/event/schemes/event_show_schema.py
@@ -1,9 +1,8 @@
 from ninja import Schema
-from pydantic import Field
 
 from .event_schema import EventSchema
 
 
 class EventShowSchema(Schema):
     id: int
-    event: EventSchema = Field(..., alias='event')
+    event: EventSchema
