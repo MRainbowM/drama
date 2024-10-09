27fe408addccd2983d79aa21927ef96348cb6a55
Мария Фалилеева <falileeva.ma@gmail.com>
2024-10-09 13:57:17 +0800

    EventSection

---
+++ frontend/src/components/EventSection/EventSection.tsx
@@ -0,0 +1,39 @@
+'use client'
+import clsx from "clsx"
+import { useState } from "react"
+import styles from './EventSection.module.scss'
+import Arrow from 'public/static/images/arrow.svg'
+
+interface EventSectionProps {
+    title: string
+}
+
+
+export default function EventSection({ title }: EventSectionProps) {
+    const [isOpen, setOpen] = useState(false)
+
+    const onClick = () => {
+        setOpen((state) => !state)
+    }
+
+
+    return (
+        <>
+            <div className={styles.section}>
+                <div className={styles.header} onClick={onClick}>
+                    <div className={styles.toggle}>
+                        <Arrow className={clsx({ [styles.open]: isOpen })} />
+                        {/* <div className={styles.toggleLine}></div>
+                        <div className={styles.toggleLine}></div> */}
+                    </div>
+                    <h3 className={styles.title}>{title}</h3>
+                </div>
+
+
+            </div>
+
+
+            
+        </>
+    )
+}
