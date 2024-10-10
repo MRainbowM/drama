5885030e8cc6fa907d4aa2d0111a2126241b7237
Мария Фалилеева <falileeva.ma@gmail.com>
2024-10-10 20:02:39 +0800

    fix map

--- frontend/src/components/Map/Map.tsx
+++ frontend/src/components/Map/Map.tsx
@@ -3,8 +3,11 @@ import styles from './Map.module.scss'
 export default function Map() {
     return (
         <div className={styles.map}>
-
-            <iframe src="https://yandex.ru/map-widget/v1/?indoorLevel=1&ll=104.293344%2C52.272651&mode=search&oid=17955001472&ol=biz&z=16.8" width="560" height="400" frameborder="1" allowfullscreen="true"></iframe>
+            <iframe
+                src="https://yandex.ru/map-widget/v1/?indoorLevel=1&ll=104.293344%2C52.272651&mode=search&oid=17955001472&ol=biz&z=16.8"
+                width="560" height="400"
+            >
+            </iframe>
         </div>
     );
 }
