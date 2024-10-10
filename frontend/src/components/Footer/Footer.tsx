a0ef0a52965af8e4d40aba30fa6ccef5d10daaf5
Мария Фалилеева <falileeva.ma@gmail.com>
2024-10-10 21:06:44 +0800

    fix footer style

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -9,9 +9,19 @@ import Logo from 'public/static/images/nd.svg'
 export default function Footer() {
     return (<>
         <footer className={styles.footer}>
-            <div className={styles.logo}>
-                <Logo />
-                <span>© 2024 Новая Драма</span>
+
+            <div className={styles.item}>
+                <span className={styles.itemTitle}>Телефон</span>
+                <a href='tel:+73952456600'>+7 (924) 530-33-36</a>
+            </div>
+            <div className={styles.item}>
+                <span className={styles.itemTitle}>Адрес</span>
+                <span>Кожова 38, Иркутск, Россия, 664022</span>
+            </div>
+
+            <div className={styles.item}>
+                <span className={styles.itemTitle}>Почта</span>
+                <a href='mailto:novaya-drama@yandex.ru'>novaya-drama@yandex.ru</a>
             </div>
 
             <div className={styles.media}>
@@ -31,6 +41,10 @@ export default function Footer() {
                     href={'https://www.facebook.com/novayadrama'}
                     target='_blank'
                 ><FB /></Link>
+            </div>
+
+            <div className={styles.logo}>
+                <span>© 2024 Новая Драма</span>
             </div>
 
         </footer>
