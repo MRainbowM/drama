54f45aa2a56eee9581149dd43da4a34a0a7aab37
Мария Фалилеева <falileeva.ma@gmail.com>
2024-10-14 16:33:06 +0800

    sam link

--- frontend/src/components/MainGridInfo/MainGridInfo.tsx
+++ frontend/src/components/MainGridInfo/MainGridInfo.tsx
@@ -1,20 +1,35 @@
+import Link from 'next/link'
 import styles from './MainGridInfo.module.scss'
 
 
 interface MainGridInfoProps {
     title: string,
-    content: React.ReactNode
+    content: React.ReactNode,
+    link?: string
 }
 
 
 export default function MainGridInfo(
-    { title, content }: MainGridInfoProps
+    { title, content, link }: MainGridInfoProps
 ) {
     return (
         <div className={styles.root}>
-            <span className={styles.title}>
-                {title}
-            </span>
+
+            {
+                link ? (
+                    <Link
+                        href={link}
+                    >
+                        <span className={styles.title}>
+                            {title}
+                        </span>
+                    </Link>
+                ) : (
+                    <span className={styles.title}>
+                        {title}
+                    </span>
+                )
+            }
             <>{content}</>
         </div>
     )
