5ad5c92b56e1485c693dcd34c2319bda9ebaa7f7
Мария Фалилеева <falileeva.ma@gmail.com>
2024-10-15 13:31:23 +0800

    update people model

--- backend/people/models/people_model.py
+++ backend/people/models/people_model.py
@@ -2,11 +2,32 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from basis.models.dates_abstract_model import DatesAbstract
+from .services.photo_path import photo_path
+from ..choices import PEOPLE_FILTER_TAG_LIST
 
 
 class People(DatesAbstract):
     """Люди театра"""
     name = models.CharField(_('Имя, фамилия'), max_length=512)
+    tag = models.CharField(
+        _('Тег'),
+        choices=PEOPLE_FILTER_TAG_LIST,
+        max_length=128,
+        help_text='Фильтр на странице "Коллектив"',
+        null=True,
+        blank=True
+    )
+    photo = models.ImageField(
+        _('Фотография'),
+        upload_to=photo_path,
+        default='',
+        null=True,
+        blank=True
+    )
+    description = models.TextField(_('Описание'), default='', blank=True)
+    birthday = models.DateField(_('Дата рождения'), null=True, blank=True)
+    position = models.CharField(_('Должность'), max_length=256, default='', blank=True)
+    education = models.CharField(_('Образование'), max_length=256, default='', blank=True)
 
     class Meta:
         verbose_name = _('Люди')
